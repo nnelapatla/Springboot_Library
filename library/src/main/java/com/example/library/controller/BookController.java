eabaf10a85a67db75ef6097a8637a2e02723e316
Navi <naveenanelapatla@gmail.com>
2024-02-19 22:39:06 -0600

    Library

---
+++ library/src/main/java/com/example/library/controller/BookController.java
@@ -0,0 +1,49 @@
+package com.example.library.controller;
+
+// BookController.java
+
+import com.example.library.model.Book;
+import com.example.library.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+// BookController.java
+import java.util.List;
+
+@RestController
+@RequestMapping("/book")
+public class BookController {
+
+    @Autowired
+    private BookService bookService;
+
+    // GET all books
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return bookService.getAllBooks();
+    }
+
+    // GET a book by ID
+    @GetMapping("/{bookId}")
+    public Book getBookById(@PathVariable Long bookId) {
+        return bookService.getBookById(bookId);
+    }
+
+    // POST a new book
+    @PostMapping
+    public Book createBook(@RequestBody Book book) {
+        return bookService.createBook(book);
+    }
+
+    // PUT/update a book by ID
+    @PutMapping("/{bookId}")
+    public Book updateBook(@PathVariable Long bookId, @RequestBody Book book) {
+        return bookService.updateBook(bookId, book);
+    }
+
+    // DELETE a book by ID
+    @DeleteMapping("/{bookId}")
+    public void deleteBook(@PathVariable Long bookId) {
+        bookService.deleteBook(bookId);
+    }
+}
